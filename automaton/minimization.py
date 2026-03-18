dec30c1189afd81431e047b701dd79feedda9b47
mathas42 <mathias.leroy@efrei.net>
2026-03-18 18:16:30 +0100

    fonction de minimisation

--- automaton/minimization.py
+++ automaton/minimization.py
@@ -1,1 +1,22 @@
 # Mathias
+from automaton import *
+
+def minimisation(self):
+    Group = {}
+    for source, transitions in self.transitions.items():
+        for symbol, targets in transitions.items():
+            if source not in Group:
+                Group[source] = {}
+            if symbol not in Group[source]:
+                Group[source][symbol] = set()
+            for target in targets:
+                if target not in self.final_states:
+                    Group[source][symbol].add("NT")
+                else :
+                    Group[source][symbol].add("T")
+
+    print(Group)
+
+
+
+
