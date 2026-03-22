e8041c62f1af13316135d360d11dffc85fd713d7
Louis <l38.lemonnier@gmail.com>
2026-03-22 17:30:02 +0100

    Ajout des fonctions dans le init pour pouvoir les utiliser

--- automaton/__init__.py
+++ automaton/__init__.py
@@ -1,3 +1,7 @@
 from .automaton import Automaton
 from .file_manager import read_automaton_from_file
 from .standardization import standardization
+from .complement import complementarization
+from .determinization import determiniser
+from .completion import completion
+from .minimization import Minimization
