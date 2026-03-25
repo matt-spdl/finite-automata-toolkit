bda0a25d227433e2aba7938209c09d0bebb79473
Mattéo <matteo.spindler17@gmail.com>
2026-03-25 11:34:48 +0100

    Add display_automaton_table function call to main.py for improved output

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-from automaton import read_automaton_from_file
+from automaton import read_automaton_from_file, display_automaton_table
 
-read_automaton_from_file("C:/Users/matte/dev/finite-automata-toolkit/automata_files/automate.exemple.txt")
+automaton = read_automaton_from_file("C:/Users/matte/dev/finite-automata-toolkit/automata_files/automate.exemple.txt")
+print(automaton)
+
+display_automaton_table(automaton)
