2b28d34a27d65ea3fedf7a55c440adcd4e5d7793
Mattéo <matteo.spindler17@gmail.com>
2026-03-25 11:04:22 +0100

    Update import statement for read_automaton function to improve clarity

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-from automaton.file_manager import read_automaton
+from automaton import read_automaton_from_file
 
-read_automaton("C:/Users/matte/dev/finite-automata-toolkit/automata_files/automate.exemple.txt")
+read_automaton_from_file("C:/Users/matte/dev/finite-automata-toolkit/automata_files/automate.exemple.txt")
