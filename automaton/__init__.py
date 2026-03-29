54d87f31489defd524b129ea68508f9a1cc0b9ce
Mattéo <matteo.spindler17@gmail.com>
2026-03-29 16:21:29 +0200

    fix: imports

--- automaton/__init__.py
+++ automaton/__init__.py
@@ -1,9 +1,8 @@
 from .automaton_class import Automaton
 from .file_manager import read_automaton_from_file
 from .display import display_automaton_table
-from .determinization import determiniser, is_determinist
+from .determinization import determinize, is_determinist
 from .standardization import standardization, is_standard
 from .completion import completion, is_complete
 from .complement import complementarization
 from .minimization import Minimization
-
