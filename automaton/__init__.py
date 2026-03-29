773ab006270b93e55592dc5b13c14e183d5bc0db
Mattéo <matteo.spindler17@gmail.com>
2026-03-29 21:08:09 +0200

    feat: add word recognition import to initialization

--- automaton/__init__.py
+++ automaton/__init__.py
@@ -6,3 +6,4 @@ from .standardization import standardization, is_standard
 from .completion import completion, is_complete
 from .complement import complementarization
 from .minimization import Minimization
+from .word_recognition import word_recognition
