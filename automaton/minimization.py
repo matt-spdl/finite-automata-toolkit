ab07c31b91bbc704b5b2b7306532ce55b6e1bb9c
Mattéo <matteo.spindler17@gmail.com>
2026-04-01 22:47:23 +0200

    refactor: rename functions for clarity and improve state separation logic

--- automaton/minimization.py
+++ automaton/minimization.py
@@ -3,229 +3,226 @@ from automaton.determinization import is_determinist
 from automaton.completion import is_complete
 from automaton.automaton_class import Automaton
 
-def separationTerminal(automaton: Automaton):
-    """
-    Sépare les états dans deux dictionnaires
-    Un dictionnaire qui regroupe tous les états terminaux avec leur cible sur des états terminaux ou non
-    Un dictionnaire qui regroupe tous les états non-terminaux avec leur cible sur des états terminaux ou non
 
-    Args:
-        un automate
-
-    Returns:
-        Deux dictionnaires de dictionnaire
+def split_terminal_nonterminal_states(automaton: Automaton):
     """
-    Group_NT = {}
-    Group_T = {}
-    for source, transitions in automaton.transitions.items():
+    Sépare les états en deux groupes :
+    - états terminaux
+    - états non terminaux
+    """
+    non_terminal_group = {}
+    terminal_group = {}
+
+    for source_state, transitions in automaton.transitions.items():
         for symbol, targets in transitions.items():
-            if source not in automaton.final_states:
-                if source not in Group_NT:
-                    Group_NT[source] = {}
-                if symbol not in Group_NT[source]:
-                    Group_NT[source][symbol] = set()
+
+            if source_state not in automaton.final_states:
+                if source_state not in non_terminal_group:
+                    non_terminal_group[source_state] = {}
+
+                if symbol not in non_terminal_group[source_state]:
+                    non_terminal_group[source_state][symbol] = set()
+
                 for target in targets:
                     if target not in automaton.final_states:
-                        Group_NT[source][symbol].add("NT")
-                    else :
-                        Group_NT[source][symbol].add("T")
-            else :
-                if source not in Group_T:
-                    Group_T[source] = {}
-                if symbol not in Group_T[source]:
-                    Group_T[source][symbol] = set()
-                for target in targets:
-                    if target not in automaton.final_states:
-                        Group_T[source][symbol].add("NT")
+                        non_terminal_group[source_state][symbol].add("NT")
                     else:
-                        Group_T[source][symbol].add("T")
-    return Group_NT, Group_T
+                        non_terminal_group[source_state][symbol].add("T")
+
+            else:
+                if source_state not in terminal_group:
+                    terminal_group[source_state] = {}
+
+                if symbol not in terminal_group[source_state]:
+                    terminal_group[source_state][symbol] = set()
+
+                for target in targets:
+                    if target not in automaton.final_states:
+                        terminal_group[source_state][symbol].add("NT")
+                    else:
+                        terminal_group[source_state][symbol].add("T")
+
+    return non_terminal_group, terminal_group
 
 
-def regroup(automaton: Automaton, Group):
+def group_states_by_transition_signature(automaton: Automaton, state_group):
     """
-    Regroupe les états terminaux (ou non) qui ont des cibles terminales ou non identiques
-
-    Args:
-        Un automate et un dictionnaire de dictionnaire
-
-    Returns:
-        Un dictionnaire qui contient des listes en valeurs
+    Regroupe les états ayant les mêmes signatures de transitions
     """
-    result = {}
-    compar = {}
-    i = 1
-    for source,values in Group.items():
+    grouped_result = {}
+    signature_map = {}
+    group_index = 1
 
-        verif = str(values)
-        if verif not in compar:
-            compar[verif] = []
-        compar[verif].append(source)
+    for source_state, transitions_signature in state_group.items():
+        signature_str = str(transitions_signature)
 
-    for verif in compar.values():
-        result[i]=verif
-        i += 1
-    return result
+        if signature_str not in signature_map:
+            signature_map[signature_str] = []
 
-def assemble(Group_NT, Group_T):
+        signature_map[signature_str].append(source_state)
+
+    for states_list in signature_map.values():
+        grouped_result[group_index] = states_list
+        group_index += 1
+
+    return grouped_result
+
+
+def merge_terminal_and_nonterminal_groups(non_terminal_groups, terminal_groups):
     """
-    Assemble les deux dictionnaires de listes des états terminaux et des états non-terminaux.
-
-    Args:
-        deux dictionnaires avec des listes en valeurs
-
-    Returns:
-        un dictionnaire avec des listes en valeurs
+    Fusionne les groupes terminaux et non terminaux
     """
-    result ={}
-    i = 1
+    merged_groups = {}
 
-    for values in Group_NT.values():
-        result[values[0]] = values
-        i += 1
+    for states_list in non_terminal_groups.values():
+        merged_groups[states_list[0]] = states_list
 
-    for values in Group_T.values():
-        result[values[0]] = values
-        i += 1
+    for states_list in terminal_groups.values():
+        merged_groups[states_list[0]] = states_list
 
-    return result
+    return merged_groups
 
-def classification(automaton: Automaton, reGroup):
+
+def classify_states_by_target_group(automaton: Automaton, current_groups):
     """
-    La fonction crée des classes (groupes) avec les groupes précédents formées
-    et regroupe  dans un dictionnaire, tous les états de l'automates avec la classe de leur cible
-
-    Args:
-        Un dictionnaire, un dictionnaire avec des listes en valeurs
-
-    Returns:
-        Un dictionnaire contenant un dictionnaire en valeur dans un set
+    Associe chaque état à la classe de ses états cibles
     """
-    result = {}
-    for source, transitions in automaton.transitions.items():
+    state_classification = {}
+
+    for source_state, transitions in automaton.transitions.items():
         for symbol, targets in transitions.items():
-            if source not in result:
-                result[source] = {}
-            if symbol not in result[source]:
-                result[source][symbol] = set()
+
+            if source_state not in state_classification:
+                state_classification[source_state] = {}
+
+            if symbol not in state_classification[source_state]:
+                state_classification[source_state][symbol] = set()
+
             for target in targets:
-                for key, group in reGroup.items():
-                    if target in group:
-                        result[source][symbol] = key
-    return (result)
+                for group_id, group_states in current_groups.items():
+                    if target in group_states:
+                        state_classification[source_state][symbol] = group_id
 
-def deleteUselessState(Group, reGroup):
+    return state_classification
+
+
+def remove_equivalent_duplicate_states(classified_states, current_groups):
     """
-    Reprend les classes formées précédemment et retire les états en doublon à condition que :
-    -Les états viennent de la même classe
-    -Les états ont les mêmes classes en cible
-
-    Args:
-         un dictionnaire avec des listes en valeurs, Un dictionnaire contenant un dictionnaire en valeur dans un set
-
-    Returns:
-        Deux dictionnaire contenant un dictionnaire en valeur dictionnaire ou chaine de caractère
+    Supprime les états équivalents (mêmes transitions et même groupe)
     """
-    result_dic = {}
-    result_str = {}
-    seen = []
+    minimized_dict = {}
+    minimized_signature = {}
+    seen_signatures = []
 
-    for source_2 in reGroup:
-        for source, values in Group.items():
-            if source in reGroup[source_2]:
-                if values not in seen:
-                    if source not in result_dic:
-                        result_dic[source]=[]
-                    result_dic[source].append(values)
-                    result_str[source]= values
-                    seen.append(values)
-        seen = []
+    for group_id in current_groups:
+        for state, signature in classified_states.items():
 
-    return result_dic, result_str
+            if state in current_groups[group_id]:
+                if signature not in seen_signatures:
+
+                    if state not in minimized_dict:
+                        minimized_dict[state] = []
+
+                    minimized_dict[state].append(signature)
+                    minimized_signature[state] = signature
+
+                    seen_signatures.append(signature)
+
+        seen_signatures = []
+
+    return minimized_dict, minimized_signature
 
 
-def breakClass(Minimize, Group, reGroup):
+def refine_partitions(minimized_signature, classified_states, current_groups):
     """
-    Casse les classes qui ont des états qui ne peuvent pas être regroupés
-
-    Args:
-         Un dictionnaire contenant un dictionnaire en valeur dans un set, un dictionnaire avec des listes en valeurs
-
-    Returns:
-        Un dictionnaire contenant un dictionnaire en valeur dans un set
+    Raffine les classes (partitionnement)
     """
-    N_reGroup = {}
+    refined_groups = {}
 
-    for source, values in Minimize.items():
-        for source2, values2 in Group.items():
-            for source3, values3 in reGroup.items():
-                if values2 == values and source2 in values3 and source in values3:
-                    if str(source) not in N_reGroup:
-                        N_reGroup[str(source)] = []
-                    N_reGroup[str(source)].append(source2)
+    for state, signature in minimized_signature.items():
+        for state2, signature2 in classified_states.items():
+            for group_id, group_states in current_groups.items():
 
-    return N_reGroup
+                if signature2 == signature and state2 in group_states and state in group_states:
+                    if str(state) not in refined_groups:
+                        refined_groups[str(state)] = []
+
+                    refined_groups[str(state)].append(state2)
+
+    return refined_groups
 
 
-def reCreateAutomaton(automaton: Automaton, Minimize):
+def build_minimized_automaton(automaton: Automaton, minimized_groups):
     """
-    Transforme notre résultat qui a la forme d'un dictionnaire en automate de notre classe
-
-    Args:
-         Un automate, Un dictionnaire contenant un dictionnaire en valeur dans un set
-
-    Returns:
-        Notre automate final minimizé
+    Reconstruit l'automate minimisé
     """
-    New_automaton = Automaton(automaton.name)
-    for source, values in Minimize.items():
-        for mini in values:
-            for symbol, targets in mini.items():
-                    New_automaton.add_transition(source, symbol, targets)
-                    if source in automaton.initial_states:
-                        New_automaton.add_initial_state(source)
-                    if source in automaton.final_states:
-                        New_automaton.add_final_state(source)
+    minimized_automaton = Automaton(automaton.name)
 
-    return New_automaton
+    for new_state, signatures in minimized_groups.items():
+        for signature in signatures:
+            for symbol, target in signature.items():
+
+                minimized_automaton.add_transition(new_state, symbol, target)
+
+                if new_state in automaton.initial_states:
+                    minimized_automaton.add_initial_state(new_state)
+
+                if new_state in automaton.final_states:
+                    minimized_automaton.add_final_state(new_state)
+
+    return minimized_automaton
+
 
 def Minimization(automaton: Automaton):
     """
-   Fonction de minimisation avec toutes les fonctions étape par étape
-
-    Args:
-         Un automate
-
-    Returns:
-        L'automate minimisé
+    Fonction principale de minimisation
     """
 
     if not is_determinist(automaton):
         raise ValueError(f"L'automate {automaton.name} n'est pas déterministe")
+
     if not is_complete(automaton):
         raise ValueError(f"L'automate {automaton.name} n'est pas complet")
 
-    Group_NT, Group_T = separationTerminal(automaton)
-    Group_NT = regroup(automaton, Group_NT)
-    Group_T = regroup(automaton, Group_T)
-    reGroup = assemble(Group_NT, Group_T)
-    Group = classification(automaton, reGroup)
-    Minimize_dic, Minimize_str = deleteUselessState(Group, reGroup)
+    non_terminal_group, terminal_group = split_terminal_nonterminal_states(automaton)
 
-    while len(Minimize_dic) != len(reGroup):
-        reGroup = breakClass(Minimize_str, Group, reGroup)
-        Group = classification(automaton, reGroup)
-        Minimize_dic, Minimize_str = deleteUselessState(Group, reGroup)
+    non_terminal_group = group_states_by_transition_signature(automaton, non_terminal_group)
+    terminal_group = group_states_by_transition_signature(automaton, terminal_group)
 
-    result = reCreateAutomaton(automaton, Minimize_dic)
+    current_groups = merge_terminal_and_nonterminal_groups(non_terminal_group, terminal_group)
+
+    classified_states = classify_states_by_target_group(automaton, current_groups)
+
+    minimized_dict, minimized_signature = remove_equivalent_duplicate_states(
+        classified_states, current_groups
+    )
+
+    while len(minimized_dict) != len(current_groups):
+        current_groups = refine_partitions(minimized_signature, classified_states, current_groups)
+
+        classified_states = classify_states_by_target_group(automaton, current_groups)
+
+        minimized_dict, minimized_signature = remove_equivalent_duplicate_states(
+            classified_states, current_groups
+        )
+
+    result = build_minimized_automaton(automaton, minimized_dict)
 
     if result == automaton:
         print("L'automate était déjà minimal.")
+
     return result
+
 
 if __name__ == "__main__":
     from automaton.file_manager import read_automaton_from_file
-    automaton = read_automaton_from_file("C:/Users/matte/dev/finite-automata-toolkit/automata_files/a44.txt")
+
+    automaton = read_automaton_from_file(
+        "C:/Users/matte/dev/finite-automata-toolkit/automata_files/a44.txt"
+    )
+
     print(automaton)
-    min_automaton = Minimization(automaton)
-    print(min_automaton)
+
+    minimized_automaton = Minimization(automaton)
+
+    print(minimized_automaton)
